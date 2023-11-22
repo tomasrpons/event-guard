429737de06843c76cd21dbda277f0b8342704eff
Juan Pedro Pont Verges <pedro@cacao.to>
2023-11-22 19:08:26 -0300

    minor fixes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import { getServerAuthSession } from "~/server/auth";
 import { redirect } from "next/navigation";
-import { Button } from "~/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -10,12 +10,22 @@ export default async function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Strela
-        </h1>
-        <div className="text-center text-lg">Dash!</div>
+    <main className="min-h-screen bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex justify-end">
+        <Link
+          href={"/api/auth/signout"}
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+        >
+          Cerrar sesión
+        </Link>
+      </div>
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Docta Cap
+          </h1>
+          <div className="text-center text-lg">Dash!</div>
+        </div>
       </div>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 import { getServerAuthSession } from "~/server/auth";
 
 export default async function Home() {
   const session = await getServerAuthSession();
-
+  
+  if (session) {
+    redirect("/dashboard");
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
