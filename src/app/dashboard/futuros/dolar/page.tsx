f085e69ced4a9e2e75aaae63b137b07f8640fc17
Juan Pedro Pont Verges <pedro@cacao.to>
2024-02-13 18:26:40 -0300

    reformated dolar layoour

--- src/app/dashboard/futuros/dolar/page.tsx
+++ src/app/dashboard/futuros/dolar/page.tsx
@@ -5,20 +5,37 @@ import { DataTable as FuturesDataTable } from "~/app/dashboard/futuros/component
 import { useStratexContext } from "~/hooks/stratex-hooks";
 import { DataTable as CurvesDataTable } from "./components/data-table";
 import { columns as CurvesColumns } from "./components/columns";
+import LineChart from "./components/line-chart";
 
 export default function Dolar() {
   const { futures } = useStratexContext();
-  const dollars = futures.filter((future) => future.ticker?.includes('DLR'))
+  const dollars = futures.filter((future) => future.ticker?.includes("DLR"));
   return (
     <div className="container">
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-4 items-center">
         <div>
           <h2 className="mb-2 text-3xl font-bold">Resumen</h2>
           <FuturesDataTable data={dollars} columns={FuturesColumns} />
         </div>
         <div>
-          <h2 className="mb-2 text-3xl font-bold">Curvas</h2>
-          <CurvesDataTable data={dollars} columns={CurvesColumns} />
+          <div className="">
+            <div className="flex justify-between">
+              <h2>Tasas implícitas (TNA Y TEA)</h2>
+              <span className="text-sm mr-4">
+                {new Date().toLocaleDateString("es-AR", {
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "2-digit",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </span>
+            </div>
+            <div>Una breve explicación???</div>
+          </div>
+          <LineChart />
+          {/* <h2 className="mb-2 text-3xl font-bold">Curvas</h2>
+          <CurvesDataTable data={dollars} columns={CurvesColumns} /> */}
         </div>
       </div>
     </div>
