2462f20ff4f5948fbb94a0c307cee21a77fd3380
Juan Pedro Pont Verges <pedro@cacao.to>
2024-02-19 14:33:21 -0300

    fixed bug

--- src/context/stratex/stratex-provider.component.tsx
+++ src/context/stratex/stratex-provider.component.tsx
@@ -10,19 +10,14 @@ interface StratexProviderProps {
 const StratexProvider: React.FC<StratexProviderProps> = (props) => {
   const { futures, stocks, bonds, dollars } = usePrimary();
   const isMarketClosed = () => {
-    const currentDateTime = new Date(
-      new Date().toLocaleString("es-AR", {
-        timeZone: "America/Argentina/Cordoba",
-      })
-    );
+    const currentDateTime = new Date().toLocaleString("en-US", { timeZone: "America/Argentina/Cordoba" });
     const currentDay = new Date(currentDateTime).getDay();
     const currentHour = new Date(currentDateTime).getHours();
-    console.log("currentDay", currentDay);
-    console.log("currentHour", currentHour);
+
     if (currentDay >= 1 && currentDay <= 5 && currentHour >= 11 && currentHour < 17) {
-      return true;
-    } else {
       return false;
+    } else {
+      return true;
     }
   };
   return (
